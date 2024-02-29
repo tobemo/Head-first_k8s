c358c7c1c4c2daa32e8d4ed5bafb46624cc959f5
tobemo <t.beers.m@gmail.com>
2024-02-29 14:42:22 +0100

    Uses a more advanced demo.

--- hello.py
+++ hello.py
@@ -1,8 +1,45 @@
-# hello.py
-from flask import Flask
+import code
+import io
+import contextlib
+import signal
+import os
+import sys
 
-app = Flask(__name__)
+import flask
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+app = flask.Flask(__name__)
+
+app.consoles = {}
+
+class WebConsole:
+
+    def __init__(self):
+        self.console = code.InteractiveConsole()
+
+    def run(self, code):
+        output = io.StringIO()
+        with contextlib.redirect_stdout(output):
+            with contextlib.redirect_stderr(output):
+                for line in code.splitlines():
+                    self.console.push(line)
+
+        return {'output': str(output.getvalue())}
+
+@app.route('/api/<uname>/run/', methods=['POST'])
+def run(uname):
+    if not uname in app.consoles:
+        app.consoles[uname] = WebConsole()
+    return flask.jsonify(
+        app.consoles[uname].run(
+            flask.request.get_json()['input']
+        )
+    )
+
+@app.route('/api/crash/', methods=['GET'])
+def crash():
+    os.kill(os.getpid(), signal.SIGTERM)
+
+# We want Flask to shutdown when requested
+signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
+
